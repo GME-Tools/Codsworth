e934e2c92f9933a90468eb060cceb203727e77a8
Benjamin Coudrin <benjamin.coudrin@gmail.com>
2022-11-11 13:13:41 +0000

    Character sheet management commands

--- commands/character.js
+++ commands/character.js
@@ -1,16 +1,28 @@
 import { EmbedBuilder, SlashCommandBuilder } from 'discord.js';
+import { Client } from "@notionhq/client"
+
+const notion = new Client({ auth: process.env.NOTION_KEY });
+const charID = process.env.CHARACTER_DB_ID;
 
 export const command = {
   data: new SlashCommandBuilder()
     .setName('character')
     .setDescription('Character sheet management')
-    /*.addSubcommand(subcommand => 
+    .addSubcommand(subcommand => 
       subcommand
-        .setName("create")
-        .setDescription("Attach a new character sheet to the current user")
-        .addStringOption(option => option.setName("Name").setDescription("Character Name").setRequired(true)))*/,
+        .setName('create')
+        .setDescription('Attach a new character sheet to the current user')
+        .addStringOption(option =>
+          option
+            .setName('name')
+            .setDescription('Character Name')
+            .setRequired(true)
+        )
+    ),
   execute: async (interaction) => {
+    const uid = interaction.user.id;
+    
     console.log(interaction)
-    //await interaction.reply("Character created")
+    await interaction.reply("Character created")
   }
 }
